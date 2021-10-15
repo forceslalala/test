0635d547d95836c1b53ae82317ccb740c775a87b
王涛 <1045257201@qq.com>
2021-10-15 14:31:15 +0800

    测试提交

---
+++ src/test1/ShowLogicErrors.java
@@ -0,0 +1,8 @@
+package test1;
+
+public class ShowLogicErrors {
+    public  static void main(String[] args){
+        System.out.print("Celsius 35 is Fasrenheit degree ");
+        System.out.println((9.0/5)*35+32);
+    }
+}
