06e73ead33188c04836d9f8264764039999ad3ed
王涛 <1045257201@qq.com>
2021-10-15 13:45:14 +0800

    测试提交

---
+++ src/test1/ShowSyntaxErrors.java
@@ -0,0 +1,9 @@
+package test1;
+
+import jdk.nashorn.internal.parser.JSONParser;
+
+public class ShowSyntaxErrors {
+    public static void main(String[] args){
+        System.out.println("Welcome to Java");
+    }
+}
